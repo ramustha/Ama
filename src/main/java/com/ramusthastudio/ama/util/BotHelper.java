17fcbf304521f4ed231180c60100e91c6d4cdaa0
ramustha <ramusthanest@gmail.com>
2017-03-16 04:29:43 +0000

    remove http

--- src/main/java/com/ramusthastudio/ama/util/BotHelper.java
+++ src/main/java/com/ramusthastudio/ama/util/BotHelper.java
@@ -64,76 +64,11 @@ public final class BotHelper {
   public static final String KW_VIDEOS = "Video";
   public static final String KW_PANDUAN = "Panduan";
 
-  private static OkHttpClient.Builder okHttpClient() {
-    LOG.info("creating ConnectionSpec ....");
-    ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-        .tlsVersions("TLSv1", "TLSv1.1", "TLSv1.2")
-        // .cipherSuites(CipherSuite.TLS_RSA_WITH_3DES_EDE_CBC_SHA)
-        // .allEnabledCipherSuites()
-        // .allEnabledTlsVersions()
-        // .cipherSuites(
-        //     CipherSuite.TLS_DH_anon_EXPORT_WITH_DES40_CBC_SHA,
-        //     CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-        //     CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-        //     CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-        //     CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-        //     // CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-        //     // CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-        //     CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-        //     CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-        //     CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-        //     CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-        //     CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256,
-        //     CipherSuite.TLS_RSA_WITH_AES_256_GCM_SHA384,
-        //     CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
-        //     CipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA,
-        //     CipherSuite.TLS_RSA_WITH_3DES_EDE_CBC_SHA
-        // )
-        .build();
-
-    LOG.info("creating CertificatePinner....");
-    CertificatePinner certificatePinner = new CertificatePinner.Builder()
-        .add("localhost", "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
-        .build();
-
-    LOG.info("creating TrustManagerFactory....");
-
-    OkHttpClient.Builder okhttp = new OkHttpClient.Builder();
-    X509TrustManager trustManager = null;
-    SSLSocketFactory sslSocketFactory = null;
-    try {
-      TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-          TrustManagerFactory.getDefaultAlgorithm());
-      trustManagerFactory.init((KeyStore) null);
-      TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-      if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-        throw new IllegalStateException("Unexpected default trust managers:"
-            + Arrays.toString(trustManagers));
-      }
-      trustManager = (X509TrustManager) trustManagers[0];
-
-      SSLContext sslContext = SSLContext.getInstance("TLS");
-      sslContext.init(null, new TrustManager[] {trustManager}, null);
-      sslSocketFactory = sslContext.getSocketFactory();
-
-      LOG.info("creating sslSocketFactory....");
-      okhttp.sslSocketFactory(sslSocketFactory, trustManager);
-    } catch (Exception aE) {
-      aE.printStackTrace();
-    }
-
-    LOG.info("creating OkHttpClient....");
-    return okhttp
-        .certificatePinner(certificatePinner)
-        .connectionSpecs(Collections.singletonList(cs));
-  }
-
   public static Response<UserProfileResponse> getUserProfile(String aChannelAccessToken,
       String aUserId) throws IOException {
     LOG.info("getUserProfile...");
     return LineMessagingServiceBuilder
         .create(aChannelAccessToken)
-        .okHttpClientBuilder(okHttpClient())
         .build().getProfile(aUserId).execute();
   }
 
@@ -144,7 +79,6 @@ public final class BotHelper {
     LOG.info("replayMessage...");
     return LineMessagingServiceBuilder
         .create(aChannelAccessToken)
-        .okHttpClientBuilder(okHttpClient())
         .build().replyMessage(pushMessage).execute();
   }
 
@@ -155,7 +89,6 @@ public final class BotHelper {
     LOG.info("pushMessage...");
     return LineMessagingServiceBuilder
         .create(aChannelAccessToken)
-        .okHttpClientBuilder(okHttpClient())
         .build().pushMessage(pushMessage).execute();
   }
 
@@ -166,7 +99,6 @@ public final class BotHelper {
     LOG.info("multicastMessage...");
     return LineMessagingServiceBuilder
         .create(aChannelAccessToken)
-        .okHttpClientBuilder(okHttpClient())
         .build().multicast(pushMessage).execute();
   }
 
@@ -177,7 +109,6 @@ public final class BotHelper {
     LOG.info("templateMessage...");
     return LineMessagingServiceBuilder
         .create(aChannelAccessToken)
-        .okHttpClientBuilder(okHttpClient())
         .build().pushMessage(pushMessage).execute();
   }
 
@@ -188,7 +119,6 @@ public final class BotHelper {
     LOG.info("stickerMessage...");
     return LineMessagingServiceBuilder
         .create(aChannelAccessToken)
-        .okHttpClientBuilder(okHttpClient())
         .build().pushMessage(pushMessage).execute();
   }
 
